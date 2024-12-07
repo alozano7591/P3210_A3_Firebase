4bdaa70a97eaed0adb6292ba3857f6b812125c44
walnut-oil <Xxdoodledoo@gmail.com>
2024-12-07 13:33:01 -0500

    comment removal

--- app/src/main/java/com/mobile2/p3210_a3/view/FavouriteDetailsActivity.java
+++ app/src/main/java/com/mobile2/p3210_a3/view/FavouriteDetailsActivity.java
@@ -69,14 +69,12 @@ public class FavouriteDetailsActivity extends AppCompatActivity {
 
         updateUI();
 
-        // NEW ADDITION
         binding.updateBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 updateMoviePlot(binding.plotTV.getText().toString());
             }
         });
-        // NEW ADDITION
 
         binding.backBtn.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -105,8 +103,7 @@ public class FavouriteDetailsActivity extends AppCompatActivity {
                 .into(posterIV);
     }
 
-    // NEW ADDITION
-    public void updateMoviePlot(String newPlot){
+    public void updateMoviePlot(String newPlot) {
         String userId = mAuth.getCurrentUser().getUid();
 
         db.collection("userMovieFavourites")
@@ -128,7 +125,6 @@ public class FavouriteDetailsActivity extends AppCompatActivity {
                     Toast.makeText(FavouriteDetailsActivity.this, "Error editing plot.", Toast.LENGTH_SHORT).show();
                 });
     }
-    // NEW ADDITION
 
     public void deleteMovieFromDb(){
         String userId = mAuth.getCurrentUser().getUid();
